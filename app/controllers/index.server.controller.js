cc2491ea859b7e914196d05758056f89a2e2c8bf
digma89 <digma89@hotmail.com>
2016-09-24 12:23:27 -0400

    V.9.0.3 routes configured

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -6,35 +6,14 @@ exports.render = function(req, res) {
     res.render('index', {
         title: "Naufal Avalúos"
     });
-
-    var data = req.body
-    var mailOptions = {
-        from: data.inputName,
-        //to: 'conaufal@yahoo.com.mx',
-        to: 'digma89@hotmail.com',
-        subject: 'Mensaje de ' + data.inputName + ' Asunto: ' + data.inputSubject,
-        text: 'Mensaje de ' + data.inputName + '\n Asunto: ' + data.inputSubject + '\n Email: ' + data.inputEmail + '\n Mensaje: ' + data.inputMessage
-    };
-
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: ' + info.response);
-    });
-
 };
-
-
-
-
 
 /**inputName
  * inputEmail
  * inputSubject
  * inputMessage
  * Send an email when the contact from is submitted
- 
+ */
 exports.sendMail = function(req, res) {
     var data = req.body
 
@@ -55,4 +34,3 @@ exports.sendMail = function(req, res) {
     res.json(data);
 
 };
-*/
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,4 +1,7 @@
-module.exports = function(app){
+module.exports = function(app) {
     var index = require('../controllers/index.server.controller');
     app.get('/', index.render);
+
+
+    app.post('/', index.sendMail);
 };
