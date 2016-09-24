a5af0918a4f074e9ea50f3f93605278aa6ed43fe
digma89 <digma89@hotmail.com>
2016-09-24 11:42:19 -0400

    V.9.0.2

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -10,10 +10,10 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
+            var name = $("input#inputName").val();
+            var email = $("input#inputEmail").val();
+            var phone = $("input#inputSubject").val();
+            var message = $("textarea#inputMessage").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
