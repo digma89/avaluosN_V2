8135bb3c78fc6ef28b1a2dd4989d129d2059e872
digma89 <digma89@hotmail.com>
2016-10-21 22:34:02 -0400

    V.10.0.0 send emails, done

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,7 +1,3 @@
-var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport('smtps://corporativonaufal%40gmail.com:naufal/avaluos1984@smtp.gmail.com');
-
-
 exports.render = function(req, res) {
     res.render('index', {
         title: "Naufal Avalúos"
@@ -15,20 +11,32 @@ exports.render = function(req, res) {
  * Send an email when the contact from is submitted
  */
 exports.sendMail = function(req, res) {
-    var data = req.body
+    var nodemailer = require('nodemailer');
+    //var transporter = nodemailer.createTransport('smtps://corporativonaufal%40gmail.com:naufal/avaluos1984@smtp.gmail.com');
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: { user: 'corporativonaufal@gmail.com', pass: 'naufal/avaluos1984' },
+        secure: true
+    });
+
+    var data = req.body;
 
     var mailOptions = {
         from: data.inputName,
-        to: 'conaufal@yahoo.com.mx',
+        to: 'digma89@hotmail.com',
         subject: 'Mensaje de ' + data.inputName + ' Asunto: ' + data.inputSubject,
         text: 'Mensaje de ' + data.inputName + '\n Asunto: ' + data.inputSubject + '\n Email: ' + data.inputEmail + '\n Mensaje: ' + data.inputMessage
     };
 
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
+            res.send('error');
             return console.log(error);
+        } else {
+            res.send('sent');
         }
-        console.log('Message sent: ' + info.response);
+
     });
 
 
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,7 +1,5 @@
 module.exports = function(app) {
     var index = require('../controllers/index.server.controller');
     app.get('/', index.render);
-
-
     app.post('/', index.sendMail);
 };
